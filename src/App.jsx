299b040289ffc9e132c48b42bd0e0b788296049b
Nataliia Maidannyk <nataliia.maydannyk@plus4u.net>
2024-07-12 13:51:25 +0300

    added react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+
+function App() {
+  return (
+    <div>
+      <h1>Щоденник твоїх успіхів!</h1>
+    </div>
+  );
+}
+
+export default App;
